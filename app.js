4a5d3d53e10455e28904df8f740e265d50ea04d1
Ishaan-11 <ishaansharma1998@gmail.com>
2020-08-15 15:59:25 +0530

    21.4.1

--- app.js
+++ app.js
@@ -9,13 +9,9 @@ app.get("/", function(req,res) {
     var currentDay = today.getDay();
 
     if(currentDay === 6 || currentDay === 0) {
-        res.send("<h1>Yay its the weekend</h1>");
+        res.sendFile(__dirname + '/weekend.html');
     } else {
-        // res.send("<h1>Boo! I have to work</h1>");
-        // res.write("<p>It is not the weekend!</p>")
-        // res.write("<h1>Boo! I have to work!</h1>");
-        // res.send();
-        res.sendFile(__dirname + "/index.html");
+        res.sendFile(__dirname + "/weekday.html");
     }
 
 });
