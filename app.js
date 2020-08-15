8dd99aedff68598468d0c92277a25a825ba61bbe
Ishaan-11 <ishaansharma1998@gmail.com>
2020-08-15 16:11:18 +0530

    21.4.2

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const bodyParser = require('body-parser');
 
 const app =express();
 
+app.set('view engine','ejs');
+
 app.get("/", function(req,res) {
-    // res.send("Hello");
+
     var today = new Date();
     var currentDay = today.getDay();
+    var day = "";
 
-    if(currentDay === 6 || currentDay === 0) {
-        res.sendFile(__dirname + '/weekend.html');
+    if(currentDay == 6 || currentDay === 0) {
+        day = "weekend";
     } else {
-        res.sendFile(__dirname + "/weekday.html");
+        day = "weekday";
     }
+
+    res.render('list', {kindOfDay: day});
 
 });
 
