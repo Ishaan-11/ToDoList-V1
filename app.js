10317eeda45304afa80c6bd079651d3f3c02aff7
Ishaan-11 <ishaansharma1998@gmail.com>
2020-08-16 16:23:21 +0530

    Understanding Templating vs Layouts - 1

--- app.js
+++ app.js
@@ -3,27 +3,38 @@ const bodyParser = require('body-parser');
 
 const app =express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.get("/", function(req,res) {
-    var today = new Date();
-    var options = {
+    let today = new Date();
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
-    var day = today.toLocaleDateString("en-US", options);
-    res.render('list', {kindOfDay: day, newListItems: items});
+    let day = today.toLocaleDateString("en-US", options);
+    res.render('list', {ListTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res) {
-    var item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    let item = req.body.newItem;
+    console.log(req.body);
+    if(req.body.list === 'Work') {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", function(req, res) {
+    res.render("list", {ListTitle: "Work List", newListItems: workItems});
 });
 
 app.listen(3000, function(){
