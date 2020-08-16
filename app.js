f42e325422e6501df61d05e3cbe9bc5b8a56299f
Ishaan-11 <ishaansharma1998@gmail.com>
2020-08-16 17:17:22 +0530

    Node Module Exports - 1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 
 const app =express();
 
@@ -11,13 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.get("/", function(req,res) {
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    let day = today.toLocaleDateString("en-US", options);
+    var day = date();
     res.render('list', {ListTitle: day, newListItems: items});
 });
 
